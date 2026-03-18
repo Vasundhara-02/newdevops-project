2bab22ad4354797fbb4722abbb1b9008ee6f6d06
Vasundhara-02 <vasundhara.techh@gmail.com>
2026-03-18 14:54:45 +0000

    fix routes

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Self-Healing Infrastructure Platform Running");
+  res.send("Self-Healing Infrastructure Platform Running 🚀");
 });
 
 app.get("/health", (req, res) => {
-  res.json({ status: "healthy" });
+  res.json({
+    status: "healthy"
+  });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
